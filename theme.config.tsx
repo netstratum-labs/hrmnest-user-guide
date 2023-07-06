068ab55b735e5e9874f6f1be787a2f72df069b76
Hadhil <hadhilg@netstratum.com>
2023-07-06 17:22:13 +0530

    header update

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>HRMNest Documentation</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="" />
+      <meta property="og:description" content="Nurture Your Most Valuable ASSET" />
+    </>
+  )
+
  
 }
 
